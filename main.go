5db67f338fb186093056f8a913297960fa1fc867
Seungkyu Ahn <seungkyua@gmail.com>
2021-10-10 13:37:32 +0900

    add PID namespace

--- main.go
+++ main.go
@@ -11,22 +11,41 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what??")
 	}
 }
 
 func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	args := append([]string{"child"}, os.Args[2:]...)
+	cmd := exec.Command("/proc/self/exe", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	//cmd.Env = []string{"PS1=[ns-process] # "}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr {
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
 
+	must(syscall.Chroot("/root/sample_rootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	must(cmd.Run())
 }
 
